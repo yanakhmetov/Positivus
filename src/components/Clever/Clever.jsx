9a6669d3c6796715dbe1f7d9abe4946742ee4aa8
Roman Yanakhmetov <11roma11.89@mail.ru>
2025-09-26 10:48:40 +0300

    fix clever

--- src/components/Clever/Clever.jsx
+++ src/components/Clever/Clever.jsx
@@ -1,11 +1,10 @@
 import React from 'react'
 import './Clever.css'
-import FirstGif from './image-clever/FirstGif.gif'
 
 function Clever(props) {
   return (
     <div className='clever-image' style={{backgroundImage: `url(${props.gif})`}}>
-        {/* <img className='clever-image' src={FirstGif} alt="clever" /> */}
+        
     </div>
   )
 }
